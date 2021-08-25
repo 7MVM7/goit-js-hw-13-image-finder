8b7613aded0180f934a49d3b3cfea44c736bb134
Vladyslaw Makhnyk <itdelive@gmail.com>
2021-08-25 18:38:45 +0300

    Done

--- src/js/imageFinder.js
+++ src/js/imageFinder.js
@@ -2,28 +2,24 @@ import template from '../templates/imageCard.hbs';
 import { searchForm, input, ul, modalDiv, modalDivButton, modalImg, addPictures } from './refs';
 import getPictures from '../helpers/apiService';
 
-// объект значений которые будут использоваться для запросов
 const state = {
   page: 1,
   query: '',
 };
 
-// вешаем слушатели событий
 searchForm.addEventListener(`submit`, sendSubmit);
 ul.addEventListener(`click`, opneModal);
 addPictures.addEventListener(`click`, addNewPictures);
 modalDiv.addEventListener(`click`, closeModalWindow);
 
-// таргеты за которыми будем следить observer
 const targets = document.getElementsByClassName('modal-img');
-// настройки нашего observer
+
 const options = {
   root: null,
   rootMargin: '0px',
   threshold: 0.5,
 };
 
-// Функция которая догружает изображения, когда срабатывает observer
 const loadImage = function () {
   if (state.page > 1) {
     state.page += 1;
@@ -35,14 +31,12 @@ const loadImage = function () {
   }
 };
 
-// инициализируем объект observer
 const observer = new IntersectionObserver(loadImage, options);
-// говорим обзерверу следить за картинкой перебирая массив всех картинок
+
 [...targets].forEach(target => {
   observer.observe(target);
 });
 
-// функция отправки сабмита
 function sendSubmit(e) {
   e.preventDefault();
   ul.innerHTML = ``;
@@ -58,7 +52,6 @@ function sendSubmit(e) {
   });
 }
 
-// при клике на кнопку load more догружаются картинки, после того как один раз на нее кликнули6 срабатывает observer
 function addNewPictures() {
   state.page += 1;
   getPictures(state.query, state.page).then(resp => {
@@ -69,7 +62,6 @@ function addNewPictures() {
   addPictures.removeAttribute('style');
 }
 
-// Функция открытия модалки
 function opneModal(e) {
   if (e.target.nodeName !== 'IMG') {
     return;
@@ -80,7 +72,6 @@ function opneModal(e) {
   modalImg.setAttribute(`src`, `${e.target.src}`);
 }
 
-// функция закрытия модалки по эскейп
 function closeByEscape(e) {
   if (e.key !== `Escape`) {
     return;
@@ -88,7 +79,6 @@ function closeByEscape(e) {
   closeModalWindow();
 }
 
-// функция закрытия модалки
 function closeModalWindow(e) {
   if (e?.target === modalImg) {
     return;
